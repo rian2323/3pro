aebd3b41f52ee47509c501e43367422e9d23dba0
riannn0123 <g14910he@gm.tsuda.ac.jp>
2016-06-22 12:48:27 +0900

    Initial Commit

---
+++ 3pro2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  3pro2
+//
+//  Created by 波多野　瑛子 on 2016/06/22.
+//  Copyright © 2016年 波多野　瑛子. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
